fd697ef945626e4fc14977d6e28cf34a55579e49
Paul Nechifor <paul@nechifor.net>
2015-09-15 20:51:14 +0100

    Add failing tests.

---
+++ test/mycode.js
@@ -0,0 +1,8 @@
+describe('packagex', function () {
+    it('should equal 2', function () {
+        (1 + 1).should.be.equal(2);
+    });
+    it('should equal 3', function () {
+        (1 + 1).should.be.equal(3);
+    });
+});
